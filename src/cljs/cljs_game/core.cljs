40158ef7d1b45753636eee4c2ae4bf73410bc795
Jonathan Tinkham <sctincman@gmail.com>
2016-12-19 08:40:53 -0700

    JS interop: try out onreadystate

--- src/cljs/cljs_game/core.cljs
+++ src/cljs/cljs_game/core.cljs
@@ -1,3 +1,12 @@
 (ns cljs-game.core)
 
-(js/alert "Welcome! From Clojurescript")
+(set! (.-onreadystatechange js/document)
+      (fn []
+        (when (= (.-readyState js/document) "complete")
+          (js/alert "Welcome!")
+          (let [element (js/document.getElementById "front-buffer")
+                context (.getContext element "2d")]
+            (js/console.log context)
+            (set! (.-fillStyle context) "green")
+            (.fillRect context 10 10 100 100)
+            ))))
