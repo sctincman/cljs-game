819da32d836005e7c0ab329250ded13ca79e1aef
Jonathan Tinkham <sctincman@gmail.com>
2017-01-03 22:55:10 -0700

    Implement game and logic loops with fixed time-step looping, and implement world-state as a global atom.

--- src/cljs/cljs_game/core.cljs
+++ src/cljs/cljs_game/core.cljs
@@ -3,6 +3,30 @@
 
 (enable-console-print!)
 
+(def world (atom {}))
+
+(defn update-world
+  [delta-time]
+  (let [mesh (@world :mesh)]
+    (set! (.-x (.-rotation mesh)) (+ 0.01 (.-x (.-rotation mesh))))
+    (set! (.-y (.-rotation mesh)) (+ 0.02 (.-y (.-rotation mesh))))
+    (swap! world assoc :mesh mesh)))
+
+(defn game-loop
+  [now]
+  (let [prev (@world :prev-time)
+        leftover-time (@world :accum-time)
+        time-step 16.666666666666668]
+    (swap! world assoc :prev-time now)
+    (swap! world assoc :leftover-time
+           (loop [accumulated (+ leftover-time (- now prev))]
+             (if (>= accumulated time-step)
+               (do (update-world time-step)
+                   (recur (- accumulated time-step)))
+               accumulated)))
+    (js/requestAnimationFrame game-loop)))
+
+
 (defn ^:export init-game
   []
   (let [scene (three/Scene.)
@@ -15,14 +39,17 @@
         light2 (three/PointLight. 0xffffff 2 0)]
     (set! (.-z (.-position camera)) 900) 
     (.add scene mesh)
+    (swap! world assoc :mesh mesh)
     (.add scene light)
     (.set (.-position light2) 300 300 300)
     (.add scene light2)
     (.setSize renderer js/window.innerWidth js/window.innerHeight)
     (js/document.body.appendChild (.-domElement renderer))
-    (let [animate (fn animate []
-         (js/requestAnimationFrame animate)
-         (set! (.-x (.-rotation mesh)) (+ 0.01 (.-x (.-rotation mesh))))
-         (set! (.-y (.-rotation mesh)) (+ 0.02 (.-y (.-rotation mesh))))
+    (swap! world assoc :prev js/Performance.now)
+    (swap! world assoc :accum-time 0.0)
+    (let [animate (fn animate [current-time]
+                    (js/requestAnimationFrame animate)
+                    (game-loop current-time)
                     (.render renderer scene camera))]
-      (animate))))
+      (animate js/Performance.now))))
+
