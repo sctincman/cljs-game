bb442cdbdc33bd76ab43ab73ab931688bb146370
Jonathan Tinkham <JonathanTinkham@fico.com>
2017-01-18 12:20:01 -0700

    Implement an orthographic view, and toggle on o key. Add second entity (cube) to emphasize this

--- src/cljs_game/core.cljs
+++ src/cljs_game/core.cljs
@@ -10,6 +10,7 @@
   (atom {:accum-time 0.0
          :prev-time 0.0
          :running true
+         :perspective true
          :entities []}))
 
 (defn update-world
@@ -45,11 +46,15 @@
                       (assoc-in [:components :render-component] (render/create-sprite-component! "assets/images/placeholder.png"))
                       (assoc-in [:components :command-component] (input/->CommandComponent nil))
                       (assoc-in [:components :input-component] (input/->InputComponent nil))
-                      (assoc-in [:components :body-component] (physics/->BodyComponent {:x 0 :y 0 :z 0} {:x 0 :y 0 :z 0})))]
+                      (assoc-in [:components :body-component] (physics/->BodyComponent {:x 0 :y 0 :z 0} {:x 0 :y 0 :z 0})))
+        test-cube (-> (ecs/->Entity 43 {})
+                      (assoc-in [:components :position-component] (ecs/->PositionComponent -400 100 20))
+                      (assoc-in [:components :render-component] (render/create-cube-component)))]
     (render/add-to-backend backend test-sprite)
+    (render/add-to-backend backend test-cube)
     (js/document.addEventListener "keydown" input/handle-input!)
     (swap! world assoc :prev-time js/Performance.now)
-    (swap! world assoc :entities [test-sprite])
+    (swap! world assoc :entities [test-cube test-sprite])
     (swap! input/input-mapping assoc "i" {:type :input
                                           :action :info
                                           :target :none
@@ -59,10 +64,15 @@
                                           :target :world
                                           :execute (fn [] (swap! world assoc :running
                                                                  (not (@world :running))))})
+    (swap! input/input-mapping assoc "o" {:type :input
+                                          :action :perspective
+                                          :target :renderer
+                                          :execute (fn [] (swap! world assoc :perspective
+                                                                 (not (@world :perspective))))})
     (let [animate (fn animate [current-time]
                     (js/requestAnimationFrame animate)
                     (game-loop! current-time)
-                    (render/render backend (:entities @world)))]
+                    (render/render backend (:entities @world) (:perspective @world)))]
       (animate js/Performance.now))))
 
 (defn on-js-reload []
--- src/cljs_game/render.cljs
+++ src/cljs_game/render.cljs
@@ -12,18 +12,20 @@
 
 (defprotocol ^:export RenderBackend
   "Abstract the details of a backend into this standard interface."
-  (render [this entities] "Using the list of entities, render and display the scene.")
+  (render [this entities perspective?] "Using the list of entities, render and display the scene.")
   (add-to-backend [this entity] "Ensure backend is aware of an entity's render-components. Eg for Three.js this does Scene.add(mesh).")
   (prepare-scene [this entities] "Perform backend specific actions needed before rendering."))
 
-(defrecord ^:export ThreeJSBackend [renderer scene camera objects]
+(defrecord ^:export ThreeJSBackend [renderer scene perspective-camera ortho-camera objects]
   RenderBackend
   (add-to-backend [this entity]
     (.add scene (get-in entity [:components :render-component :backend :mesh]))
     entity)
-  (render [this entities]
+  (render [this entities perspective?]
     (let [entities (doall (prepare-scene this entities))]
-      (.render renderer scene camera)
+      (if perspective?
+        (.render renderer scene perspective-camera)
+        (.render renderer scene ortho-camera))
       entities))
   (prepare-scene [this entities]
     (let [renderables (filter renderable? entities)
@@ -40,18 +42,23 @@
 
 (defn ^:export create-threejs-backend! []
   (let [scene (three/Scene.)
-        camera (three/PerspectiveCamera. 75 (/ js/window.innerWidth js/window.innerHeight) 0.1, 1000)
+        camera (three/PerspectiveCamera. 75 (/ js/window.innerWidth js/window.innerHeight) 0.1 1000)
+        ortho-camera (three/OrthographicCamera. (/ js/window.innerWidth -2)
+                                                (/ js/window.innerWidth 2)
+                                                (/ js/window.innerHeight 2)
+                                                (/ js/window.innerHeight -2) 0.1 1000)
         renderer (three/WebGLRenderer.)
         light (three/AmbientLight. 0x404040)
         light2 (three/PointLight. 0xffffff 2 0)]
     (set! (.-z (.-position camera)) 900)
+    (set! (.-z (.-position ortho-camera)) 900)
     (.add scene light)
     (set! (.-background scene) (three/Color. 0x6c6c6c))
     (.set (.-position light2) 300 300 300)
     (.add scene light2)
     (.setSize renderer js/window.innerWidth js/window.innerHeight)
     (js/document.body.appendChild (.-domElement renderer))
-    (->ThreeJSBackend renderer scene camera [light light2])))
+    (->ThreeJSBackend renderer scene camera ortho-camera [light light2])))
 
 (defn ^:export create-cube-component []
   (let [geometry (three/BoxGeometry. 200 200 200)
