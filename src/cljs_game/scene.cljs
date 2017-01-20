c1d166291a09fbae5cd6a84efe5810c3dc3c5f50
Jonathan Tinkham <sctincman@gmail.com>
2017-01-19 21:19:16 -0700

    Add test background image, and shell of scene subsystem

---
+++ src/cljs_game/scene.cljs
@@ -0,0 +1,9 @@
+(ns cljs-game.scene
+  (:require [cljs-game.entity :as ecs]
+            [clojure.data.json :as json]
+            [ajax.core :as ajax]))
+
+(defrecord Scene [])
+
+(defn load-entity! [name]
+  )
